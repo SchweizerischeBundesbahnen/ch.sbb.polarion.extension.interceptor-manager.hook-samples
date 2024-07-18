11c201c6712909ce5dff9bd918fb5359ebd4d055
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2024-07-18 13:20:21 +0200

    feat: example for module comment hook added
    Interceptor v2.3.0 required

---
+++ hook-samples/delete-non-resolved-module-comments/src/main/java/ch/sbb/polarion/extension/interceptor/hook_samples/delete_non_resolved_module_comments/DeleteNonResolvedModuleCommentsHook.java
@@ -0,0 +1,42 @@
+package ch.sbb.polarion.extension.interceptor.hook_samples.delete_non_resolved_module_comments;
+
+import ch.sbb.polarion.extension.interceptor.model.ActionHook;
+import ch.sbb.polarion.extension.interceptor.model.HookExecutor;
+import ch.sbb.polarion.extension.interceptor.util.PropertiesUtils;
+import com.polarion.alm.tracker.model.IModuleComment;
+import com.polarion.core.util.logging.Logger;
+import com.polarion.platform.persistence.model.IPObject;
+import org.jetbrains.annotations.NotNull;
+
+@SuppressWarnings("unused")
+public class DeleteNonResolvedModuleCommentsHook extends ActionHook implements HookExecutor {
+
+    public static final String DESCRIPTION = "Allow removing only non-resolved comments from document.";
+
+    public static final Logger logger = Logger.getLogger(DeleteNonResolvedModuleCommentsHook.class);
+
+    public DeleteNonResolvedModuleCommentsHook() {
+        super(ItemType.MODULE_COMMENT, ActionType.DELETE, DESCRIPTION);
+    }
+
+    @Override
+    public String preAction(@NotNull IPObject object) {
+        IModuleComment moduleComment = (IModuleComment) object;
+
+        if (moduleComment.isResolvedComment()) {
+            return "'Resolved' comments can not be deleted.";
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public @NotNull HookExecutor getExecutor() {
+        return this; //there is no need to create a separate executor instance coz only 'pre' action used
+    }
+
+    @Override
+    public String getDefaultSettings() {
+        return PropertiesUtils.build();
+    }
+}
