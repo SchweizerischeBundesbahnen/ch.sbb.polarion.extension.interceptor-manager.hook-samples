5df5c55e5c844a3e90be093d5bd6acb0505f99a9
e550448 <andrei.shakirin@sbb.ch>
2024-09-17 13:01:42 +0200

    fix: Fixed code smell
    Refs: #31

--- hook-samples-osgi/delete-non-resolved-module-comments/src/main/java/ch/sbb/polarion/extension/interceptor/hook_samples/osgi/HookServlet.java
+++ hook-samples-osgi/delete-non-resolved-module-comments/src/main/java/ch/sbb/polarion/extension/interceptor/hook_samples/osgi/HookServlet.java
@@ -1,8 +1,10 @@
 package ch.sbb.polarion.extension.interceptor.hook_samples.osgi;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class HookServlet extends HttpServlet {
 
@@ -10,8 +12,13 @@ public class HookServlet extends HttpServlet {
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) {
         try {
             getServletContext().getRequestDispatcher("/hookExample.jsp").forward(req, resp);
-        } catch (Exception e) {
-            throw new IllegalStateException("Get request processing failure: " + e.getMessage(), e);
+        } catch (ServletException | IOException e) {
+            try {
+                log("doGet failed: " + e.getMessage(), e);
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An internal error occurred");
+            } catch (IOException ex) {
+                log("IOException occurred by sending error response: "+ e.getMessage(), e);
+            }
         }
     }
 }
