5058fcc63607c349969dd6c4b9fac2a86420af28
e550448 <andrei.shakirin@sbb.ch>
2024-09-17 12:25:48 +0200

    fix: Fixed vulnerability
    Refs: #31

--- hook-samples-osgi/delete-non-resolved-module-comments/src/main/java/ch/sbb/polarion/extension/interceptor/hook_samples/osgi/HookServlet.java
+++ hook-samples-osgi/delete-non-resolved-module-comments/src/main/java/ch/sbb/polarion/extension/interceptor/hook_samples/osgi/HookServlet.java
@@ -1,27 +1,18 @@
 package ch.sbb.polarion.extension.interceptor.hook_samples.osgi;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class HookServlet extends HttpServlet {
 
-    /* (non-Javadoc)
-     * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
-     */
     @Override
-    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/hookExample.jsp").forward(req, resp);
-    }
-
-    /* (non-Javadoc)
-     * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
-     */
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) {
+        try {
+            getServletContext().getRequestDispatcher("/hookExample.jsp").forward(req, resp);
+        } catch (Exception e) {
+            throw new RuntimeException("Get request processing failure: " + e.getMessage(), e);
+        }
     }
 }
 
