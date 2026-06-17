d2155c8aa63bbcad2bc7a524641d3337c20cc608
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2026-06-17 13:59:30 +0200

    feat!: Polarion 2606 support
    - Migrate servlet imports from javax to jakarta in HookServlet.java
    - Update pom.xml to use jakarta.servlet-api version 6.1.0
    
    Refs: #80

--- hook-samples-guice/delete-non-resolved-module-comments/src/main/java/ch/sbb/polarion/extension/interceptor/hook_samples/guice/HookServlet.java
+++ hook-samples-guice/delete-non-resolved-module-comments/src/main/java/ch/sbb/polarion/extension/interceptor/hook_samples/guice/HookServlet.java
@@ -1,8 +1,8 @@
 package ch.sbb.polarion.extension.interceptor.hook_samples.guice;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class HookServlet extends HttpServlet {
--- hook-samples-osgi/delete-non-resolved-module-comments/src/main/java/ch/sbb/polarion/extension/interceptor/hook_samples/osgi/HookServlet.java
+++ hook-samples-osgi/delete-non-resolved-module-comments/src/main/java/ch/sbb/polarion/extension/interceptor/hook_samples/osgi/HookServlet.java
@@ -1,9 +1,9 @@
 package ch.sbb.polarion.extension.interceptor.hook_samples.osgi;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class HookServlet extends HttpServlet {
